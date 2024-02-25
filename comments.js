615dfeefc4a0018b73ea06f1b71da9d7ad2cd53a
AlfredoGuadalupeAG <160700135+AlfredoGuadalupeAG@users.noreply.github.com>
2024-02-25 20:40:39 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,70 @@
+// Create web server
+const express = require('express');
+const app = express();
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+// Create web server
+app.get('/comments', (req, res) => {
+    res.send('This is a web server');
+});
